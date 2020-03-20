2121bc4ac85f8fb5f1e44e3af9c13270b8d8cced
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-03-19 22:05:47 -0700

    finish stock and recipe batches

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,29 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    min_prices = min(prices[0], prices[1])
+    profit = prices[1] - prices[0]
+
+    if len(prices) > 2:
+        for i in range(2, len(prices)):
+            if prices[i] - min_prices > profit:
+                profit = prices[i] - min_prices
+            if prices[i] < min_prices:
+                min_prices = prices[i]
+    return profit
+
+    pass
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
